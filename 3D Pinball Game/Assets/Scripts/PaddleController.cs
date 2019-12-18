c40e6133bbd45fe45db7b1cf6ef59d43830f8030
liambansal <55756009+liambansal@users.noreply.github.com>
2019-12-18 11:56:38 +0000

    Basic Pinabll Concept
    Implemented the paddles and pinabll along with a basic board.

---
+++ 3D Pinball Game/Assets/Scripts/PaddleController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour {
+	[SerializeField]
+	private string paddlePrefix = "";
+
+	private Rigidbody paddleRigidbody = null;
+
+	private float forceMultiplier = 30;
+
+	private void Start() {
+		paddleRigidbody = GetComponent<Rigidbody>();
+	}
+
+	void Update() {
+        if (Input.GetAxis(paddlePrefix + "Vertical") > 0) {
+			paddleRigidbody.AddForce((Vector2.up * forceMultiplier), ForceMode.Impulse);
+		}
+    }
+}
