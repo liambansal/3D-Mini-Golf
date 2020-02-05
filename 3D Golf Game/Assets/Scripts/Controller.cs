3a1eb38b244544729bd1d918738d2f5d02847502
liambansal <55756009+liambansal@users.noreply.github.com>
2020-02-05 12:47:48 +0000

    Initial Commit
    Added:
    - Basic input for moving the ball.
    - Mouse input for moving the camera.
    - Controls for rotating the camera around the ball.
    - Simple materials.

---
+++ 3D Golf Game/Assets/Scripts/Controller.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Controller : MonoBehaviour {
+	private int forceMultiplier = 40;
+
+	private float hitForce = 0;
+	private float minHitForce = 0;
+	private float maxHitForce = 500;
+
+	private Rigidbody rigidBody = null;
+
+	private void Awake() {
+		rigidBody = GetComponent<Rigidbody>();
+	}
+
+	private void Start() {
+		Mathf.Clamp(hitForce, minHitForce, maxHitForce);
+    }
+
+    private void Update() {
+        if (Input.GetKey(KeyCode.Space)) {
+			hitForce = (forceMultiplier * (40 * Time.deltaTime));
+		}
+
+		if (Input.GetKeyUp(KeyCode.Space)) {
+			PuttBall();
+		}
+	}
+
+	private void PuttBall() {
+		rigidBody.AddForce((Vector3.forward * hitForce), ForceMode.Impulse);
+	}
+}
---
+++ 3D Golf Game/Assets/Scripts/MainCamera.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class MainCamera : MonoBehaviour {
+	[SerializeField]
+	private GameObject golfBall = null;
+
+	private float translationBuffer = 10;
+	private float xTranslation = 0;
+	private float yTranslation = 0;
+
+	//private Vector3 offset;
+
+	//private void Start() {
+	//	offset = transform.position - golfBall.transform.position;
+	//}
+
+	private void Update() {
+		//transform.position = offset + golfBall.transform.position;
+
+		if ((Input.GetAxis("Mouse X") > 0) || (Input.GetAxis("Mouse X") < 0)) {
+			xTranslation = (Input.GetAxis("Mouse X") * translationBuffer);
+			transform.RotateAround(golfBall.transform.position, Vector3.up, xTranslation);
+		}
+
+		if ((Input.GetAxis("Mouse Y") > 0) || (Input.GetAxis("Mouse Y") < 0)) {
+			yTranslation = (Input.GetAxis("Mouse Y") * translationBuffer);
+			transform.RotateAround(golfBall.transform.position, transform.right, -yTranslation);
+		}
+
+		transform.LookAt(golfBall.transform, transform.up);
+		Mathf.Clamp(transform.rotation.x, 0, 70);
+	}
+}
